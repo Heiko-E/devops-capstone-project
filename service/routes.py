ef9ec4fa88d1fa3038b15987144cbc5d3c093ad2
Heiko-E <hegerter@posteo.de>
2023-01-15 13:31:19 +0000

    updates according to hints

--- service/routes.py
+++ service/routes.py
@@ -66,10 +66,9 @@ def list_accounts():
     app.logger.info("Request to list all Accounts")
     check_content_type("application/json")
     accounts = Account.all()
-    message = []
-    for account in accounts:
-        message.append(account.serialize())
-    return jsonify(message), status.HTTP_200_OK
+    account_list = [account.serialize() for account in accounts]
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -225,3 +225,9 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(resp.get_json())
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        
