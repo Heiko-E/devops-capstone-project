0d461a88fe96cd352fe1fb407d84c2c34a0a25ce
Heiko-E <hegerter@posteo.de>
2025-09-13 13:56:49 +0200

    Fix foramting

--- service/config.py
+++ service/config.py
@@ -13,7 +13,8 @@ if not DATABASE_URI:
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "postgres")
     DATABASE_NAME = os.getenv("DATABASE_NAME", "postgres")
     DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-    DATABASE_URI = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:5432/{DATABASE_NAME}"
+    DATABASE_URI = (f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@"
+                    f"{DATABASE_HOST}:5432/{DATABASE_NAME}")
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
